105a5bea11336e3f806b6cbf586486f64b732dce
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-17 16:32:59 +0700

    ✨ Cropped context on App component

--- client-side/src/index.tsx
+++ client-side/src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { CartContextProvider } from "./store/cart-context";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
+  </React.StrictMode>,
 );
-
-
