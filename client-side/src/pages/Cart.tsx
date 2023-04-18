0de80589dd873eacdf606993007a8a57ee673d4d
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-18 16:51:47 +0700

    💄 Added a sentence on Cart

--- client-side/src/pages/Cart.tsx
+++ client-side/src/pages/Cart.tsx
@@ -4,6 +4,7 @@ import CartContext from "../store/cart-context";
 import ImageEmptyBox from "../assets/empty-box.jpeg";
 
 import CartItems from "../components/CartItems";
+import { Link } from "react-router-dom";
 
 interface CartProps {}
 
@@ -13,10 +14,18 @@ const Cart: FC<CartProps> = ({}) => {
   return (
     <div className="pt-40">
       {cart.length <= 0 && (
-        <div>
-          <h1 className="pt-10 text-center text-3xl font-bold ">
-            Your cart is empty.
-          </h1>
+        <div className="text-center">
+          <h1 className="pt-10 text-3xl font-bold ">Your cart is empty.</h1>
+          <p className="pt-10">
+            No item?{" "}
+            <Link
+              className="text-amber-500 hover:text-amber-600 underline"
+              to="/login"
+            >
+              Login
+            </Link>{" "}
+            to see items you may have added from another computer or device.
+          </p>
           <img className="mx-auto mt-10" src={ImageEmptyBox} alt="Empty box" />
         </div>
       )}
