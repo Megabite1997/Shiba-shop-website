1f67c3f7315871fbf615a808c1305defea0ed7d9
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-21 00:39:33 +0700

    🔥 Removed Accessories component

--- client-side/src/pages/Products/Accessories.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-
-interface AccessoriesProps {}
-
-const Accessories: FC<AccessoriesProps> = ({}) => {
-  return <div>Accessories</div>;
-};
-
-export default Accessories;
