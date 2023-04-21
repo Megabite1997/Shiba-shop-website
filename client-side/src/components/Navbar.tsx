5f982b91e04e0d225bb41df14d6bb137777582c6
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-21 18:53:11 +0700

    🎨 Improved structure Navbar

--- client-side/src/components/Navbar.tsx
+++ client-side/src/components/Navbar.tsx
@@ -10,14 +10,6 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 const Navbar: React.FC = () => {
   const CartCtx = useContext(CartContext);
 
-  const listNavbarLinks = [
-    { name: "Products", path: "products" },
-    { name: "About Us", path: "about-us" },
-    { name: "Contact Us", path: "contact-us" },
-    { name: "Login / Register", path: "login" },
-    { name: "Cart", path: "cart" },
-  ];
-
   return (
     <header className="fixed w-full flex bg-antiquewhite items-center justify-end p-4 shadow-lg">
       <Link className="mr-auto" to="/">
@@ -30,31 +22,6 @@ const Navbar: React.FC = () => {
 
       <nav className="hidden md:block">
         <ul className="list-none m-0 p-0 flex w-full gap-20 mr-10 items-center">
-          {/* {listNavbarLinks.map((element, index) => ( */}
-          {/* <li className="text-lg font-medium" key={index}>
-            <NavLink
-              to={element.path}
-              className={({ isActive }) =>
-                isActive
-                  ? "flex items-center gap-2 text-amber-600 "
-                  : "flex items-center gap-2 hover:text-amber-600"
-              }
-            >
-              {element.path === "login" && <VscAccount />}
-              {element.path === "cart" && (
-                <>
-                  <div className="bg-white py-1 px-3 rounded-full">
-                    {CartCtx.cart.length}
-                  </div>
-                  <AiOutlineShoppingCart />
-                </>
-              )}
-              {element.name}
-              {element.path === "products" && <RiArrowDropDownLine size={30} />}
-            </NavLink>
-          </li> */}
-          {/* ))} */}
-
           <li className="text-lg font-medium">
             <NavLink
               to="products"
