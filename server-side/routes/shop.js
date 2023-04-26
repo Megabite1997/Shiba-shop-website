f908f9bb69c833b6084197dfed8e01d844af9fb9
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-26 14:09:24 +0700

    🎨 create path for using mongoose

--- server-side/routes/shop.js
+++ server-side/routes/shop.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { getProducts, createProduct } = require("../mongo");
-
+const mongooseMiddleware = require('../mongoose')
 
 router.get("/", getProducts);
+
+router.get('/with-mongoose', mongooseMiddleware.createProduct)
 
 router.post("/", createProduct);
 
