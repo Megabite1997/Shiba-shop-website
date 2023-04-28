6c1ecd21bc75e6413f1c4370430f937960ddcfbf
Tada Chaipanya <fendersdtua@hotmail.com>
2023-04-28 10:26:26 +0700

    🎨 Created Footer

---
+++ client-side/src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineCopyright } from "react-icons/ai";
+
+interface FooterProps {}
+
+const Footer: FC<FooterProps> = ({}) => {
+  return (
+    <footer className="mt-auto grid grid-flow-col min-[320px]:text-ss md:text-base bg-red-500 justify-around p-4">
+      <div className="flex items-center">
+        <p>Copyright</p>
+        <AiOutlineCopyright />
+        <p>2023 Tada & Shiba Inc. All rights reserved</p>
+      </div>
+
+      <div className="flex">
+        <Link
+          className="border-r border-white px-4 hover:underline"
+          to="policy"
+        >
+          Privacy Policy
+        </Link>
+        <Link className="border-r border-white px-4 hover:underline" to="terms">
+          Terms of Use
+        </Link>
+        <Link
+          className="border-r border-white px-4 hover:underline"
+          to="sales-and-refunds"
+        >
+          Sales and Refunds
+        </Link>
+        <Link className="border-r border-white px-4 hover:underline" to="legal">
+          Legal
+        </Link>
+        <Link className="px-4 hover:underline" to="site-map">
+          Site Map
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
