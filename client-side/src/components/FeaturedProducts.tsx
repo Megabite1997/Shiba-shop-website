503f3e7ef477fbc3678db1a0122aa3cd5b4c8d0c
Tada Chaipanya <fendersdtua@hotmail.com>
2023-05-09 02:14:38 +0700

    🎨 Created "FeaturedProducts" component

---
+++ client-side/src/components/FeaturedProducts.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+interface FeaturedProductsProps {}
+
+const FeaturedProducts: FC<FeaturedProductsProps> = ({}) => {
+  return (
+    <div>
+      <h2>Featured products</h2>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
