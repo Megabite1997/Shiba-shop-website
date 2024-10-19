0f5085608821c98276e2b323e1beed470e507622
Tada Chaipanya <fendersdtua@hotmail.com>
2024-10-19 18:41:01 +0700

    Create custom input component

---
+++ client-side/src/components/InputField.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import { FieldError, UseFormRegister } from "react-hook-form";
+
+interface InputFieldProps {
+  label: string;
+  name: string;
+  type: string;
+  register: UseFormRegister<any>; // Register from react-hook-form
+  error?: FieldError; // Error handling
+  placeholder?: string;
+  maxLength?: number;
+  className?: string;
+  labelClassName?: string;
+}
+
+const InputField: FC<InputFieldProps> = ({
+  label,
+  name,
+  type,
+  register,
+  error,
+  placeholder = "Enter text",
+  maxLength = 30,
+  className,
+  labelClassName,
+}) => {
+  return (
+    <div>
+      {label && (
+        <label className={`ml-1 ${labelClassName || ""}`}>{label}</label>
+      )}
+      <input
+        type={type}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        className={`border border-slate-400 rounded-2xl w-full p-2 pl-5 mt-2 ${className}`}
+        {...register(name)}
+      />
+      {error && (
+        <p className={`text-red-500 text-sm pt-2 pl-2 ${labelClassName}`}>
+          {error.message} !
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default InputField;
