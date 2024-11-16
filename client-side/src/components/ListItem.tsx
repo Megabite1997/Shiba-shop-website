c82d6152d827b6ebb946594593fb6c096c5c6496
Tada Chaipanya <fendersdtua@hotmail.com>
2024-11-16 13:05:56 +0700

    add path

--- client-side/src/components/ListItem.tsx
+++ client-side/src/components/ListItem.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 
 interface ListItemProps {
-  category: { name: string; image: string };
+  category: { name: string; image: string; path: string };
 }
 
 const ListItem: FC<ListItemProps> = ({ category }) => {
   return (
-    <div className="w-57 h-35 bg-slate-300 mr-3 border border-slate-300">
+    <Link
+      to={`products/${category.path}`}
+      className="w-57 h-35 bg-slate-300 mr-3 border border-slate-300"
+    >
       <img className="object-cover w-full h-full" src={category.image} alt="" />
       <p className="text-center pt-2">{category.name}</p>
-    </div>
+    </Link>
   );
 };
 
